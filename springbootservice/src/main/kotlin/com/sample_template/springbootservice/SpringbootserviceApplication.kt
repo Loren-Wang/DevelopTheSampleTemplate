b10009581374a3d6271c7795fe9a1c593aa380ab
LorenWang <wangliang_workjob@163.com>
2019-11-29 14:57:10 +0800

    初始化springboot样例代码

---
+++ springbootservice/src/main/kotlin/com/sample_template/springbootservice/SpringbootserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.sample_template.springbootservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootserviceApplication>(*args)
+}
